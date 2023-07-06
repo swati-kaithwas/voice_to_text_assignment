b38454580d48862ff3f18709814f04b998a71b67
swati-kaithwas <swatikaithwas8@gmail.com>
2023-07-06 14:33:28 +0530

    voice to text

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import VoiceToText from './components/VoiceToText';
+
+function App() {
+  return (
+    <div className="App">
+    <VoiceToText/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/VoiceToText.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import './VoiceToText.css';
+
+function VoiceToText() {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+
+  const handleStartListening = () => {
+    SpeechRecognition.startListening({ continuous: true });
+  };
+
+  const handleStopListening = () => {
+    SpeechRecognition.stopListening();
+  };
+
+  return (
+    <div className="voice-to-text-container">
+      <h2>Voice to Text</h2>
+      <div className="transcript-area">
+        <p className="transcript">{transcript}</p>
+      </div>
+      <div className="button-group">
+        <button className="action-button start-button" onClick={handleStartListening}>Start</button>
+        <button className="action-button stop-button" onClick={handleStopListening}>Stop</button>
+        <button className="action-button reset-button" onClick={resetTranscript}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default VoiceToText;
